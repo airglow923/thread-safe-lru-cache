724a5b498d511ebfdc2faf36a2a65e486a6cfaa7
airglow923 <p.hyundeok76@gmail.com>
2021-04-01 17:10:49 +0800

    Remove unnecessary files

--- include/hyundeok/types.h
+++
@@ -1,8 +0,0 @@
-#ifndef HYUNDEOK_UTILS_TYPES_H
-#define HYUNDEOK_UTILS_TYPES_H
-
-#include <stddef.h>
-
-typedef unsigned long long UllT;
-
-#endif
--- include/hyundeok/utils/math.h
+++
@@ -1,8 +0,0 @@
-#ifndef HYUNDEOK_UTILS_MATH_H
-#define HYUNDEOK_UTILS_MATH_H
-
-#include "hyundeok/types.h"
-
-UllT GetPrimeNumber(UllT lower_bound);
-
-#endif
